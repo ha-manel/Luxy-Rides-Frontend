e63f0f696ca58212d452d93f492e1ffaba76a128
ha-manel <manelhammouche3@gmail.com>
2022-09-22 16:25:43 +0100

    Dispatch register action

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,45 +1,70 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { register } from '../../Redux/User/registerSlice';
 
-const Register = () => (
-  <div className="container-fluid w-50 mx-auto mt-5">
-    <h2 className="h2 mt-5 mb-3 text-center">Sign Up</h2>
-    <form>
-      <div className="form-floating mb-3">
-        <input
-          type="email"
-          className="form-control"
-          id="floatingInput"
-          placeholder="name@example.com"
-          required
-        />
-        <label htmlFor="floatingInput">Email address</label>
-      </div>
-      <div className="form-floating mb-3">
-        <input
-          type="text"
-          className="form-control"
-          id="floatingName"
-          placeholder="Name"
-          required
-        />
-        <label htmlFor="floatingName">Name</label>
-      </div>
-      <div className="form-floating mb-3">
-        <input
-          type="text"
-          className="form-control"
-          id="floatingUsername"
-          placeholder="Username"
-          required
-        />
-        <label htmlFor="floatingUsername">Username</label>
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Sign Up
-      </button>
-    </form>
-  </div>
-);
+const Register = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    if (email && name && username) {
+      dispatch(register({ email, name, username }));
+      setEmail('');
+      setName('');
+      setUsername('');
+    }
+  };
+
+  return (
+    <div className="container-fluid w-50 mx-auto mt-5">
+      <h2 className="h2 mt-5 mb-3 text-center">Sign Up</h2>
+      <form onSubmit={formSubmit}>
+        <div className="form-floating mb-3">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="floatingName"
+            placeholder="Name"
+            value={name}
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="floatingName">Name</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="floatingUsername"
+            placeholder="Username"
+            value={username}
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="floatingUsername">Username</label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
