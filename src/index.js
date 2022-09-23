96c3adcaae7c1d57c4d2e83508e8a41ae9cce1a0
sarazaToussaint <tmsaraza@gmail.com>
2022-09-23 11:19:18 +0200

    Create routes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './Redux/configureStore';
+import { BrowserRouter } from 'react-router-dom'; 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter> 
+        <App />
+      </BrowserRouter> 
     </Provider>
   </React.StrictMode>,
 );
