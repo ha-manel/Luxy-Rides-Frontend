a9c3ae21c8d3ff7b1ae07f3add47f5bf58e01513
ha-manel <manelhammouche3@gmail.com>
2022-09-23 14:22:30 +0100

    Add sidebar and splash render conditions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Splash from './Components/SplashScreen/Splash';
 import Register from './Components/Register/Register';
 import Login from './Components/Login/Login';
 import Sidebar from './Components/SideBar/Sidebar';
 
 function App() {
+  const user = useSelector((state) => state.user);
   return (
     <>
-      <Sidebar />
+      {user.logged_in && <Sidebar />}
       <main className="mainCnt">
         <Routes>
-          <Route path="/" element={<Splash />} />
+          {!user.logged_in && <Route path="/" element={<Splash />} />}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Register />} />
         </Routes>
--- src/Components/SplashScreen/Splash.jsx
+++ src/Components/SplashScreen/Splash.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Splash = () => (
-  <div className="container-fluid d-flex align-items-center justify-content-center">
-    <NavLink to="login">
-      <button type="button" className="btn btn-primary">Log in</button>
-    </NavLink>
-    <NavLink to="signup">
-      <button type="button" className="btn btn-success">Sign up</button>
-    </NavLink>
+  <div
+    className="container-fluid d-flex align-items-center justify-content-center vw-100"
+    style={{ marginLeft: '-260px' }}
+  >
+    <div>
+      <NavLink to="login">
+        <button type="button" className="btn btn-primary mx-3">
+          Log in
+        </button>
+      </NavLink>
+      <NavLink to="signup">
+        <button type="button" className="btn btn-success mx-3">
+          Sign up
+        </button>
+      </NavLink>
+    </div>
   </div>
 );
 
