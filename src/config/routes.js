be9fd9cca2f13d01ca5b697f5759bb771f0b6f26
ha-manel <manelhammouche3@gmail.com>
2022-09-26 23:55:00 +0100

    Add reserve route

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ import Home from '../Components/Home/Home';
 import Login from '../Components/Login/Login';
 import PrivateRoutes from '../Components/PrivateRoutes/PrivateRoutes';
 import Register from '../Components/Register/Register';
+import Reserve from '../Components/Reserve/Reserve';
 import Splash from '../Components/SplashScreen/Splash';
 
 const routes = [
@@ -24,10 +25,10 @@ const routes = [
         path: '/home',
         element: <Home />,
       },
-      // {
-      //   path: '/reserve',
-      //   element: <Reserve />,
-      // },
+      {
+        path: '/reserve',
+        element: <Reserve />,
+      },
       // {
       //   path: '/reservations',
       //   element: <Reservations />,
