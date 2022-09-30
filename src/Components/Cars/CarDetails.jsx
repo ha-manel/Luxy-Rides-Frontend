f55d3d4d78a6a460b1e6a855b32e7b86944a0c07
sarazaToussaint <tmsaraza@gmail.com>
2022-09-30 19:47:48 +0200

    Add details page feature

--- src/Components/Cars/CarDetails.jsx
+++ src/Components/Cars/CarDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import styles from './carDetails.module.css';
 
 const CarDetails = () => {
   const { id } = useParams();
@@ -14,11 +15,20 @@ const CarDetails = () => {
 
   return (
     <>
-      <h1>Car Details</h1>
-      <div>
-        <img src={car.picture} alt="car" />
-        <p>{car.model}</p>
-        <p>{car.driver_name}</p>
+      <div className={styles.detailContainer}>
+        <div className={styles.car}>
+          <img src={car.picture} alt={car.model} />
+        </div>
+        <div className={styles.detail}>
+          <h2>{car.model}</h2>
+          <div className={styles.carDriver}>
+            <p>Driver Name:</p>
+            <p>{car.driver_name}</p>
+          </div>
+          <div className={styles.btncontainer}>
+            <button type="button" className={styles.reservebtn}>Reserve</button>
+          </div>
+        </div>
       </div>
     </>
   );
